424d68b35c29de3d41ee3d6d0c16a10e6a0cf1b1
Cumbie <bcumbie@una.edu>
2025-12-01 10:32:35 -0600

    ak;dfasjak;fj

--- app.mjs
+++ app.mjs
@@ -2,9 +2,6 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
-
-
-
 const app = express()
 // const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
